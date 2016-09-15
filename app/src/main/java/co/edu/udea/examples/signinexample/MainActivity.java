64a8212b8623cb1b6ff033221d2dfb2194560e6e
mgochoa <mochoa1127@gmail.com>
2016-09-15 11:57:50 -0500

    First

---
+++ app/src/main/java/co/edu/udea/examples/signinexample/MainActivity.java
@@ -0,0 +1,157 @@
+package co.edu.udea.examples.signinexample;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.IdentityProviders;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "MainActivity";
+    private static final String KEY_IS_RESOLVING = "is_resolving";
+    private static final String KEY_CREDENTIAL = "key_credential";
+    private static final String KEY_CREDENTIAL_TO_SAVE = "key_credential_to_save";
+
+    private static final int RC_SIGN_IN = 1;
+    private static final int RC_CREDENTIALS_READ = 2;
+    private static final int RC_CREDENTIALS_SAVE = 3;
+
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+    private boolean mIsResolving = false;
+    private Credential mCredential;
+    private Credential mCredentialToSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Build GoogleApiClient, don't set account name
+        buildGoogleApiClient(null);
+
+        // Sign in button
+        SignInButton signInButton = (SignInButton) findViewById(R.id.button_google_sign_in);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(this);
+
+        // Other buttons
+
+        findViewById(R.id.button_google_revoke).setOnClickListener(this);
+        findViewById(R.id.button_google_sign_out).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_google_sign_in:
+                onGoogleSignInClicked();
+                break;
+            case R.id.button_google_revoke:
+                onGoogleRevokeClicked();
+                break;
+            case R.id.button_google_sign_out:
+                onGoogleSignOutClicked();
+                break;
+        }
+
+    }
+    private void buildGoogleApiClient(String accountName) {
+        GoogleSignInOptions.Builder gsoBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail();
+
+        if (accountName != null) {
+            gsoBuilder.setAccountName(accountName);
+        }
+
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.stopAutoManage(this);
+        }
+
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gsoBuilder.build());
+
+        mGoogleApiClient = builder.build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    private void handleGoogleSignIn(GoogleSignInResult gsr) {
+        Log.d(TAG, "handleGoogleSignIn:" + (gsr == null ? "null" : gsr.getStatus()));
+
+        boolean isSignedIn = (gsr != null) && gsr.isSuccess();
+        if (isSignedIn) {
+            // Display signed-in UI
+            GoogleSignInAccount gsa = gsr.getSignInAccount();
+            String status = String.format("Signed in as %s (%s)", gsa.getDisplayName(),
+                    gsa.getEmail());
+            ((TextView) findViewById(R.id.text_google_status)).setText(status);
+
+        } else {
+            // Display signed-out UI
+            ((TextView) findViewById(R.id.text_google_status)).setText("Signed out");
+        }
+
+        findViewById(R.id.button_google_sign_in).setEnabled(!isSignedIn);
+        findViewById(R.id.button_google_sign_out).setEnabled(isSignedIn);
+        findViewById(R.id.button_google_revoke).setEnabled(isSignedIn);
+    }
+    private void onGoogleSignInClicked() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    private void onGoogleRevokeClicked() {
+        if (mCredential != null) {
+            Auth.CredentialsApi.delete(mGoogleApiClient, mCredential);
+        }
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        handleGoogleSignIn(null);
+                    }
+                });
+    }
+
+    private void onGoogleSignOutClicked() {
+        Auth.CredentialsApi.disableAutoSignIn(mGoogleApiClient);
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        handleGoogleSignIn(null);
+                    }
+                });
+    }
+}
